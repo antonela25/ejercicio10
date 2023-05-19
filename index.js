145c0ef44ed138164a7a46d599110e44de8c3935
antonela25 <130667643+antonela25@users.noreply.github.com>
2023-05-19 20:42:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var nombre= "Antonela";
+var apellido= "Troncoso";
+ console.log(nombre + " " + apellido)
+
+ var numero1=8;
+ var numero2=2;
+ var resultadoSuma= numero1+numero2;
+ console.log("la suma es igual a " + resultadoSuma)
+
+ var numero3=3;
+
+ var resultado= numero3 + resultadoSuma;
+
+ console.log("el resultado final es: "+ resultado)
+
+
